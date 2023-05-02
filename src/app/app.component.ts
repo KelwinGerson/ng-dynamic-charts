c8281bb94d3064d9dcdd44625716c8d80a790b9b
KelwinGerson <kelwingerson@outlook.com>
2023-05-02 13:11:24 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,50 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-dynamic-charts';
+
+  selectedChartType!: string;
+  chartTypes: string[] = ['bar-vertical', 'pie-chart'];
+  charts: any[] = [];
+  selectedDataset: any;
+  datasets: any[] = [
+    {
+      name: 'Produtos',
+      values: [
+        { name: 'Produto A', value: 100 },
+        { name: 'Produto B', value: 200 },
+        { name: 'Produto C', value: 50 },
+        { name: 'Produto D', value: 150 },
+      ],
+    },
+    {
+      name: 'Vendas',
+      values: [
+        { name: 'Janeiro', value: 150 },
+        { name: 'Fevereiro', value: 230 },
+        { name: 'Março', value: 120 },
+        { name: 'Abril', value: 175 },
+      ],
+    },
+    {
+      name: 'Categorias',
+      values: [
+        { name: 'Eletrônicos', value: 300 },
+        { name: 'Roupas', value: 250 },
+        { name: 'Acessórios', value: 150 },
+      ],
+    },
+  ];
+
+  addChart() {
+    if (this.selectedChartType && this.selectedDataset) {
+      this.charts.push({
+        type: this.selectedChartType,
+        data: this.selectedDataset.values
+      });
+    }
+  }
+
+  removeChart(index: number) {
+    this.charts.splice(index, 1);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { FormsModule } from '@angular/forms';
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxChartsModule,
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
